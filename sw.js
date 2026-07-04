2b45514e90811be25cd376a7779c966f9591e445
studyengus-tech <studyeng.us@gmail.com>
2026-07-05 05:22:01 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='stage-clock-v14';
+const CACHE='stage-clock-v16';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon-maskable-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE&&k!=='fonts').map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
